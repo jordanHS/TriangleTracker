1b80912d6333347364bff7c84b1e9ac118a0e3a5
Jordan Safford and Marcin Szopik <marcin@>
2020-02-25 20:36:27 -0800

    make triangle tracker application with Jest tsting

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css'
-import { pingPong } from './ping-pong';
+import { triangleTracker } from './triangle';
 
 $(document).ready(function() {
   $('#ping-pong-form').submit(function(event) {
     event.preventDefault();
     var goal = $('#goal').val();
-    var output = pingPong(goal);
+    var output = triangleTracker(goal);
     output.forEach(function(element) {
       $('#solution').append("<li>" + element + "</li>");
     });
